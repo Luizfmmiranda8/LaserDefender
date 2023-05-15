c02566ab84879b6378ea3303aafe8fb1be9dd023
Luizfmmiranda8 <luizfmmiranda@gmail.com>
2023-05-15 11:16:24 -0300

    Effects
    Create and implement a particle system effect to explosion and camera shake effect to trigger whenever the player get hit

--- LaserDefender/Assets/Scripts/Health.cs
+++ LaserDefender/Assets/Scripts/Health.cs
@@ -7,9 +7,18 @@ public class Health : MonoBehaviour
     #region VARIABLES
     [Header("Health")]
     [SerializeField] int health = 50;
+
+    [Header("Effects")]
+    [SerializeField] ParticleSystem hitEffect;
+    [SerializeField] bool applyCameraShake;
+    CameraShake cameraShake;
     #endregion
 
     #region EVENTS
+    void Awake()
+    {
+        cameraShake = Camera.main.GetComponent<CameraShake>();
+    }
     void OnTriggerEnter2D(Collider2D other) 
     {
         DamageDealer damageDealer = other.GetComponent<DamageDealer>();
@@ -17,6 +26,8 @@ public class Health : MonoBehaviour
         if(damageDealer != null)
         {
             TakeDamage(damageDealer.GetDamage());
+            PlayHitEffect();
+            ShakeCamera();
             damageDealer.Hit();
         }
     }
@@ -30,6 +41,23 @@ public class Health : MonoBehaviour
         if(health <= 0)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void PlayHitEffect()
+    {
+        if(hitEffect != null)
+        {
+            ParticleSystem hitEffectInstance = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(hitEffectInstance.gameObject, hitEffectInstance.main.duration + hitEffectInstance.main.startLifetime.constantMax);
+        }
+    }
+
+    void ShakeCamera()
+    {
+        if(cameraShake != null && applyCameraShake)
+        {
+            cameraShake.Play();
         }
     }
     #endregion
